6018e1da7f2d9702318e70175e346ecbc84aa399
unfoldedRoses <ptest5557@gmail.com>
2025-02-23 21:10:52 +0530

    unauth navbar validation added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/NavBar";
-// import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/SideBar";
 // import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
@@ -46,7 +46,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
-                    
+                    <Sidebar/>
                     </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
